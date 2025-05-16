04bbe792a4ca8345ed3c27c7368bb38c20a4a448
Azam Sadarat <azam.sedarat@google.com>
2025-05-16 12:34:10 +0330

    feat: add linting, formatting and initial test setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    testEnvironment: 'node',
+  };
+  
